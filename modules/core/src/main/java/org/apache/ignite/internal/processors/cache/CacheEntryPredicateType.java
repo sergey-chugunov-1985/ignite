810d147c6925bb22459ef6eb061e64bf98fcd58b
Sergey Chugunov <sergey.chugunov@gmail.com>
2026-03-20 10:27:14 +0300

    IGNITE-28264 Add license header, improve javadocs

--- modules/core/src/main/java/org/apache/ignite/internal/processors/cache/CacheEntryPredicateType.java
+++ modules/core/src/main/java/org/apache/ignite/internal/processors/cache/CacheEntryPredicateType.java
@@ -1,27 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.ignite.internal.processors.cache;
 
 /**
- * Common predicate type.
+ * Type of {@link CacheEntryPredicateAdapter}.
  */
 public enum CacheEntryPredicateType {
     /**
-     * Other custom predicate.
-     */
-    OTHER,
-    /**
-     * Entry has certain equal value.
+     * Predicate that entry has specified value.
      */
     VALUE,
     /**
-     * Entry has any value.
+     * Predicate that entry has any value.
      */
     HAS_VALUE,
     /**
-     * Entry has no value.
+     * Predicate that entry has no value.
      */
     HAS_NO_VALUE,
     /**
-     * Is always false.
+     * Predicate that is always {@code false}.
      */
-    ALWAYS_FALSE
+    ALWAYS_FALSE,
+    /**
+     * Any other predicate.
+     */
+    OTHER
 }
